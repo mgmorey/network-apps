0e945cffac5443f79f53bfccc23d8b60b0b71d3d
Michael G. Morey <mgmorey@gmail.com>
2021-05-24 18:39:03 -0500

    Refactor test program.

--- test-network.cpp
+++ test-network.cpp
@@ -3,33 +3,30 @@
 #include <cstdlib>
 #include <iostream>
 
+static void printAddresses(const Addresses& addrs,
+                           const std::string& str)
+{
+    std::cout << str << std::endl << std::endl;
+
+    for (Addresses::const_iterator it = addrs.begin();
+         it != addrs.end();
+         ++it)
+    {
+        std::cout << *it << std::endl;
+    }
+
+    std::cout << std::endl;
+}
+
 int main(void)
 {
     Address host = getHostname();
     Addresses addrs = getAddresses();
     Addresses addrs4 = getAddresses(PF_INET);
     Addresses addrs6 = getAddresses(PF_INET6);
-    std::cout << "Hostname:\t" << host << std::endl;
-    std::cout << std::endl << "All Addresses:" << std::endl << std::endl;
-
-    for (Addresses::const_iterator it = addrs.begin(); it != addrs.end(); ++it)
-    {
-        std::cout << *it << std::endl;
-    }
-
-    std::cout << std::endl << "IPv4 Addresses:" << std::endl << std::endl;
-
-    for (Addresses::const_iterator it = addrs4.begin(); it != addrs4.end(); ++it)
-    {
-        std::cout << *it << std::endl;
-    }
-
-    std::cout << std::endl << "IPv6 Addresses:" << std::endl << std::endl;
-
-    for (Addresses::const_iterator it = addrs6.begin(); it != addrs6.end(); ++it)
-    {
-        std::cout << *it << std::endl;
-    }
-
+    std::cout << "Hostname:\t" << host << std::endl << std::endl;
+    printAddresses(addrs, "All Addresses:");
+    printAddresses(addrs4, "IPv4 Addresses:");
+    printAddresses(addrs6, "IPv6 Addresses:");
     return EXIT_SUCCESS;
 }
