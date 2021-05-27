aaf677a073472b6dff12d34a9f61f6a1cf92d95d
Michael G. Morey <mgmorey@gmail.com>
2021-05-27 16:09:55 -0500

    Encapsulate struct addrinfo information in class AddrInfo.

--- network.cpp
+++ network.cpp
@@ -6,7 +6,100 @@
 #include <unistd.h>		// gethostname()
 
 #include <cstdlib>		// std::calloc()
+#include <cstring>		// std::memcpy(), std::strdup()
 #include <iostream>		// std::cerr, std::endl
+
+AddrInfo::AddrInfo()
+{
+    init();
+}
+
+AddrInfo::AddrInfo(struct addrinfo& new_ai)
+{
+    init();
+    copy(new_ai);
+}
+
+AddrInfo::AddrInfo(const AddrInfo& addrInfo)
+{
+    init();
+    copy(addrInfo);
+}
+
+AddrInfo::~AddrInfo()
+{
+    cleanup();
+}
+
+AddrInfo& AddrInfo::operator=(const struct addrinfo& new_ai)
+{
+    cleanup();
+    copy(new_ai);
+    return *this;
+}
+
+AddrInfo& AddrInfo::operator=(const AddrInfo& addrInfo)
+{
+    ai = addrInfo.ai;
+    return *this;
+}
+
+struct sockaddr* AddrInfo::getAddr() const
+{
+    return ai.ai_addr;
+}
+
+socklen_t AddrInfo::getAddrLen() const
+{
+    return ai.ai_addrlen;
+}
+
+void AddrInfo::copy(const struct addrinfo& new_ai)
+{
+    ai.ai_flags = new_ai.ai_flags;
+    ai.ai_family = new_ai.ai_family;
+    ai.ai_socktype = new_ai.ai_socktype;
+    ai.ai_protocol = new_ai.ai_protocol;
+    ai.ai_addrlen = new_ai.ai_addrlen;
+    ai.ai_addr = static_cast<sockaddr*>
+        (std::calloc(sizeof(char), ai.ai_addrlen));
+
+    if (ai.ai_addr != NULL) {
+        std::memcpy(ai.ai_addr, new_ai.ai_addr, ai.ai_addrlen);
+    }
+
+    if (new_ai.ai_canonname != NULL) {
+        ai.ai_canonname = strdup(new_ai.ai_canonname);
+    }
+    else {
+        ai.ai_canonname = NULL;
+    }
+
+    ai.ai_next = NULL;
+}
+
+void AddrInfo::cleanup()
+{
+    free(ai.ai_addr);
+    free(ai.ai_canonname);
+}
+
+void AddrInfo::copy(const AddrInfo& addrInfo)
+{
+    copy(addrInfo.ai);
+}
+
+void AddrInfo::init()
+{
+    ai.ai_flags = 0;
+    ai.ai_family = 0;
+    ai.ai_socktype = 0;
+    ai.ai_protocol = 0;
+    ai.ai_addrlen = 0;
+    ai.ai_addr = 0;
+    ai.ai_canonname = NULL;
+    ai.ai_next = NULL;
+}
 
 Addresses getAddresses(int family, int flags)
 {
@@ -15,7 +108,6 @@ Addresses getAddresses(int family, int flags)
 
 Addresses getAddresses(const Address& host, int family, int flags)
 {
-    struct addrinfo* list = NULL;
     struct addrinfo hints = {
         flags,
         family,
@@ -26,8 +118,24 @@ Addresses getAddresses(const Address& host, int family, int flags)
         NULL,
         NULL
     };
-    int error = getaddrinfo(host.c_str(), NULL, &hints, &list);
-    Addresses addrs;
+    AddrInfos addrInfos = getAddrInfo(host, &hints);
+    Addresses addresses;
+
+    for (AddrInfos::const_iterator it = addrInfos.begin();
+         it != addrInfos.end();
+         ++it) {
+        Address address = getNameInfo(*it, NI_NUMERICHOST);
+        addresses.insert(address);
+    }
+
+    return addresses;
+}
+
+AddrInfos getAddrInfo(const Address& host, struct addrinfo* hints)
+{
+    struct addrinfo* list = NULL;
+    int error = getaddrinfo(host.c_str(), NULL, hints, &list);
+    AddrInfos result;
 
     if (error) {
         std::cerr << "getaddrinfo() returned " << error
@@ -38,13 +146,22 @@ Addresses getAddresses(const Address& host, int family, int flags)
         for (const struct addrinfo* elem = list;
              elem != NULL;
              elem = elem->ai_next) {
-            addrs.insert(getNameInfo(*elem, NI_NUMERICHOST));
+            struct addrinfo ai;
+            ai.ai_flags = elem->ai_flags;
+            ai.ai_family = elem->ai_family;
+            ai.ai_socktype = elem->ai_socktype;
+            ai.ai_protocol = elem->ai_protocol;
+            ai.ai_addrlen = elem->ai_addrlen;
+            ai.ai_addr = elem->ai_addr;
+            ai.ai_canonname = NULL;
+            ai.ai_next = NULL;
+            result.push_back(ai);
         }
 
         freeaddrinfo(list);
     }
 
-    return addrs;
+    return result;
 }
 
 HostName getHostname()
@@ -62,12 +179,12 @@ HostName getHostname()
     return host;
 }
 
-NameInfo getNameInfo(const struct addrinfo& ai, int flags)
+NameInfo getNameInfo(const AddrInfo& addrInfo, int flags)
 {
     char* buffer = static_cast<char*>(std::calloc(sizeof(char), NI_MAXHOST));
     socklen_t length = sizeof(char) * NI_MAXHOST;
-    int error = getnameinfo(ai.ai_addr,
-                            ai.ai_addrlen,
+    int error = getnameinfo(addrInfo.getAddr(),
+                            addrInfo.getAddrLen(),
                             buffer, length,
                             NULL, 0,
                             flags);
--- network.h
+++ network.h
@@ -6,18 +6,44 @@
 
 #include <set>			// std::set
 #include <string>		// std::string
+#include <vector>
 
 typedef std::string Address;
+typedef class AddrInfo AddrInfo;
 typedef std::set<Address> Addresses;
+typedef std::vector<AddrInfo> AddrInfos;
 typedef std::string HostName;
 typedef std::string NameInfo;
+
+class AddrInfo
+{
+public:
+    AddrInfo();
+    AddrInfo(struct addrinfo& new_ai);
+    AddrInfo(const AddrInfo& addrInfo);
+    ~AddrInfo();
+    AddrInfo& operator=(const struct addrinfo& new_ai);
+    AddrInfo& operator=(const AddrInfo& addrInfo);
+    struct sockaddr* getAddr() const;
+    socklen_t getAddrLen() const;
+
+private:
+    void cleanup();
+    void copy(const struct addrinfo& new_ai);
+    void copy(const AddrInfo& addrInfo);
+    void init();
+
+    struct addrinfo ai;
+};
 
 Addresses getAddresses(int family = PF_UNSPEC,
                        int flags = 0);
 Addresses getAddresses(const Address& host,
                        int family = PF_UNSPEC,
                        int flags = 0);
+AddrInfos getAddrInfo(const Address& host,
+                      struct addrinfo* hints = NULL);
 HostName getHostname();
-NameInfo getNameInfo(const struct addrinfo& ai, int flags = 0);
+NameInfo getNameInfo(const AddrInfo& addrInfo, int flags);
 
 #endif
