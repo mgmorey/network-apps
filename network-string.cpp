5f63af893b9b21da97a86c88d03dd36b0879b35a
Michael G. Morey <mgmorey@gmail.com>
2021-06-02 15:29:03 -0500

    Remove redundant include directives from source files.

--- network-string.cpp
+++ network-string.cpp
@@ -1,23 +1,6 @@
-#ifdef _WIN32
-#include <winsock2.h>		// closesocket(), connect(),
-                                // gethostname(), socket()
-#include <ws2tcpip.h>		// NI_MAXHOST, struct addrinfo,
-                                // getaddrinfo(), getnameinfo(),
-                                // socklen_t
-#else
-#include <netdb.h>		// NI_MAXHOST, struct addrinfo,
-                                // getaddrinfo(), getnameinfo()
-#include <sys/socket.h>		// AF_UNSPEC, connect(), socket(),
-                                // socklen_t
-#include <unistd.h>		// close(), gethostname()
-#endif
+#include "network-string.h"
 
-#include <cassert>		// assert()
-#include <cerrno>		// errno
-#include <cstdlib>		// free()
-#include <cstring>		// std::memcpy(), strdup(),
-                                // std::strerror()
-#include <iostream>		// std::cerr, std::endl
+#include <string>		// std::string
 
 void trim_zeros(std::string& str)
 {
