c35c110de62b55541cf02af58b7019adefc3457d
Michael G. Morey <mgmorey@gmail.com>
2021-06-28 16:50:50 -0500

    Remove unused include directives from network-host.h.

--- include/network-host.h
+++ include/network-host.h
@@ -6,14 +6,10 @@
 #include "network-types.h"      // Hostname, Service
 
 #ifdef _WIN32
-#include <winsock2.h>   // struct sockaddr
-#include <ws2tcpip.h>   // struct addrinfo, socklen_t
+#include <ws2tcpip.h>   // struct addrinfo
 #else
 #include <netdb.h>      // struct addrinfo
-#include <sys/socket.h> // struct sockaddr, socklen_t
 #endif
-
-#include <string>       // std::string
 
 namespace Network
 {
