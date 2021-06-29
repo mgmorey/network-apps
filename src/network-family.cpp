f5aee90cd5ae4de1abb648fcb0661a0d56dd15e8
Michael G. Morey <mgmorey@gmail.com>
2021-06-29 16:30:59 -0500

    Update include directive inline comments.

--- src/network-family.cpp
+++ src/network-family.cpp
@@ -1,9 +1,11 @@
 #include "network-family.h"     // Family
 
 #ifdef _WIN32
-#include <winsock2.h>   // AF_INET, AF_INET6, PF_INET, PF_INET6
+#include <winsock2.h>   // AF_INET, AF_INET6, AF_UNIX, AF_UNSPEC,
+                        // PF_INET, PF_INET6, PF_UNIX, PF_UNSPEC
 #else
-#include <sys/socket.h> // AF_INET, AF_INET6, PF_INET, PF_INET6
+#include <sys/socket.h> // AF_INET, AF_INET6, AF_UNIX, AF_UNSPEC,
+                        // PF_INET, PF_INET6, PF_UNIX, PF_UNSPEC
 #endif
 
 Network::Family::Family(int family) :
@@ -15,14 +17,17 @@ std::ostream& Network::operator<<(std::ostream& os,
                                   const Family& family)
 {
     switch(family.value) {
+    case AF_UNSPEC:
+        os << "AF_UNSPEC";
+        break;
+    case AF_UNIX:
+        os << "AF_UNIX";
+        break;
     case AF_INET:
         os << "AF_INET";
         break;
     case AF_INET6:
         os << "AF_INET6";
-        break;
-    case AF_UNSPEC:
-        os << "AF_UNSPEC";
         break;
     default:
         os << family.value;
