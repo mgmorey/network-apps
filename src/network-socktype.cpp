8183149c858c9562f01f902a0588dc642bc39c6e
Michael G. Morey <mgmorey@gmail.com>
2021-06-29 19:27:59 -0500

    Include support for additional socket types in operator<<().

--- src/network-socktype.cpp
+++ src/network-socktype.cpp
@@ -3,8 +3,12 @@
 #ifdef _WIN32
 #include <winsock2.h>   // SOCK_DGRAM, SOCK_STREAM
 #else
-#include <sys/socket.h> // SOCK_DGRAM, SOCK_STREAM
+#include <sys/socket.h> // SOCK_DGRAM, SOCK_DCCP, SOCK_PACKET,
+                        // SOCK_RAW, SOCK_RDM, SOCK_SEQPACKET,
+                        // SOCK_STREAM
 #endif
+
+#include <sstream>      // std::ostringstream
 
 Network::SockType::SockType(int socktype) :
     value(socktype)
@@ -14,15 +18,71 @@ Network::SockType::SockType(int socktype) :
 std::ostream& Network::operator<<(std::ostream& os,
                                   const SockType& socktype)
 {
-    switch(socktype.value) {
-    case SOCK_DGRAM:
-        os << "SOCK_DGRAM";
+#ifndef _WIN32
+    static struct values {
+        int flag;
+        const char* label;
+    } values[] = {
+        {SOCK_CLOEXEC,              "SOCK_SLOEXEC"},
+        {SOCK_NONBLOCK,             "SOCK_NONBLOCK"},
+        {0,                         NULL}
+    };
+#endif
+    std::ostringstream oss;
+    std::size_t i = 0;
+
+    if (socktype.value & 00000017) {
+        switch (socktype.value & 00000017) {
+        case SOCK_STREAM:
+            oss << "SOCK_STREAM";
+            break;
+        case SOCK_DGRAM:
+            oss << "SOCK_DGRAM";
+            break;
+        case SOCK_RAW:
+            oss << "SOCK_RAW";
+            break;
+        case SOCK_RDM:
+            oss << "SOCK_RDM";
+            break;
+        case SOCK_SEQPACKET:
+            oss << "SOCK_SEQPACKET";
+            break;
+#ifndef _WIN32
+        case SOCK_DCCP:
+            oss << "SOCK_DCCP";
+            break;
+        case SOCK_PACKET:
+            oss << "SOCK_PACKET";
+            break;
+#endif
+        default:
+            oss << socktype.value;
+        }
+        ++i;
+    }
+
+#ifndef _WIN32
+    for(const struct values* p = values; p->flag; ++p) {
+        if (socktype.value & p->flag) {
+            if (i++ > 0) {
+                oss << " | ";
+            }
+
+            oss << p->label;
+        }
+    }
+#endif
+
+    switch (i) {
+    case 0:
+        os << 0;
         break;
-    case SOCK_STREAM:
-        os << "SOCK_STREAM";
+    case 1:
+        os << oss.str();
         break;
     default:
-        os << socktype.value;
+        os << '(' << oss.str() << ')';
     }
 
     return os;
