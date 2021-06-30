feec2c1f39f8958d3aa8267332d534d610c62572
Michael G. Morey <mgmorey@gmail.com>
2021-06-30 07:19:02 -0500

    Define enumerated type fd_null in network-fd.h.

--- include/network-fd.h
+++ include/network-fd.h
@@ -8,8 +8,10 @@
 namespace Network
 {
 #ifdef _WIN32
+    enum { fd_null = static_cast<SOCKET>(INVALID_SOCKET) };
     typedef SOCKET fd_type;
 #else
+    enum { fd_null = -1 };
     typedef int fd_type;
 #endif
 }
