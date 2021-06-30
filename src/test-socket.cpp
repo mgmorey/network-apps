86c58734742dceb42163af656b734b11b3b57d73
Michael G. Morey <mgmorey@gmail.com>
2021-06-30 09:17:51 -0500

    Utilize types FdPair and Host in source file test-socket.cpp.

--- src/test-socket.cpp
+++ src/test-socket.cpp
@@ -1,4 +1,6 @@
 #include "network-close.h"      // close()
+#include "network-host.h"       // Host
+
 #include "network-socket.h"     // Socket
 
 #include <sys/socket.h> // AF_UNIX, SOCK_DGRAM, SOCK_STREAM
@@ -10,25 +12,28 @@ static void test_socket()
 {
     Network::Socket socket(AF_UNIX, SOCK_STREAM, 0);
     Network::SocketPairResult socketpair_result(socket.socketpair(true));
-    Network::SocketPair socket_pair(socketpair_result.first);
-    Network::Result result = socketpair_result.second;
+    Network::Result result(socketpair_result.second);
 
     if (result.nonzero()) {
         std::cerr << result.string()
                   << std::endl;
     }
     else {
-        std::cout << "Connected socket "
-                  << socket_pair.first
-                  << " to socket "
-                  << socket_pair.second
-                  << std::endl;
-        Network::close(socket_pair.second);
-        Network::close(socket_pair.first);
-        std::cout << "Closed sockets "
-                  << socket_pair.first
+        Network::FdPair fds(socketpair_result.first);
+        std::cout << "Opened sockets "
+                  << fds.first
                   << " and "
-                  << socket_pair.second
+                  << fds.second
+                  << std::endl;
+        Network::Host host(socket);
+        Network::Result first_result(host.connect(fds.first, true));
+        Network::Result second_result(host.connect(fds.second, true));
+        Network::close(fds.first);
+        Network::close(fds.second);
+        std::cout << "Closed sockets "
+                  << fds.first
+                  << " and "
+                  << fds.second
                   << std::endl;
     }
 }
