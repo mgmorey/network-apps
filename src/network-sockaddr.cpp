cebb1afe6297f0028696155dcb9cfd2122e5ce37
Michael G. Morey <mgmorey@gmail.com>
2021-10-05 18:49:24 -0500

    Refactor free functions get_length() and get_sockaddr().

--- src/network-sockaddr.cpp
+++ src/network-sockaddr.cpp
@@ -24,12 +24,12 @@ static constexpr Network::SockAddr::size_type get_capacity()
 }
 
 #ifndef _WIN32
-static Network::socklen_type get_length(const sockaddr_un* addr_ptr)
+static unsigned char get_length(const sockaddr_un* addr_ptr)
 {
 #ifdef HAVE_SOCKADDR_SA_LEN
-    return static_cast<Network::socklen_type>(SUN_LEN(addr_ptr));
+    return static_cast<unsigned char>(SUN_LEN(addr_ptr));
 #else
-    return static_cast<Network::socklen_type>(sizeof *addr_ptr);
+    return static_cast<unsigned char>(sizeof *addr_ptr);
 #endif
 }
 #endif
@@ -76,15 +76,17 @@ Network::SockAddr Network::get_sockaddr(const sockaddr* addr_ptr,
 Network::SockAddr Network::get_sockaddr(const sockaddr_in* addr_ptr,
                                         socklen_type addr_len)
 {
+    const auto ptr {reinterpret_cast<const sockaddr*>(addr_ptr)};
     assert(addr_len == sizeof *addr_ptr);
-    return get_sockaddr(reinterpret_cast<const sockaddr*>(addr_ptr), addr_len);
+    return get_sockaddr(ptr, addr_len);
 }
 
 Network::SockAddr Network::get_sockaddr(const sockaddr_in6* addr_ptr,
                                         socklen_type addr_len)
 {
+    const auto ptr {reinterpret_cast<const sockaddr*>(addr_ptr)};
     assert(addr_len == sizeof *addr_ptr);
-    return get_sockaddr(reinterpret_cast<const sockaddr*>(addr_ptr), addr_len);
+    return get_sockaddr(ptr, addr_len);
 }
 
 #ifndef _WIN32
@@ -92,29 +94,25 @@ Network::SockAddr Network::get_sockaddr(const sockaddr_in6* addr_ptr,
 Network::SockAddr Network::get_sockaddr(const sockaddr_un* addr_ptr,
                                         socklen_type addr_len)
 {
-    const auto data {reinterpret_cast<const sockaddr*>(addr_ptr)};
-    const auto size {get_length(addr_ptr)};
-    assert(addr_len == size);
-    return get_sockaddr(data, size);
+    const auto ptr {reinterpret_cast<const sockaddr*>(addr_ptr)};
+    assert(addr_len == sizeof *addr_ptr);
+    return get_sockaddr(ptr, addr_len);
 }
 
 Network::SockAddr Network::get_sockaddr(const Pathname& path)
 {
-    sockaddr_un sun = {
+    sockaddr_un sun;
+    const auto len {sizeof sun.sun_path - 1};
+    const auto str {path.substr(0, len)};
+    const auto ptr {str.c_str()};
+    std::strncpy(sun.sun_path, ptr, len);
+    sun.sun_family = AF_UNIX;
 #ifdef HAVE_SOCKADDR_SA_LEN
-        sizeof sun,
+    sun.sun_len = get_length(&sun);
+#else
+    sun.sun_len = sizeof sun;
 #endif
-        AF_UNIX,
-        ""
-    };
-    const auto size {sizeof sun.sun_path};
-    std::strncpy(sun.sun_path, path.c_str(), size);
-    sun.sun_path[size - 1] = '\0';
-    const auto length {get_length(&sun)};
-#ifdef HAVE_SOCKADDR_SA_LEN
-    sun.sun_len = length;
-#endif
-    return get_sockaddr(&sun, length);
+    return get_sockaddr(&sun, sizeof sun);
 }
 
 #endif
