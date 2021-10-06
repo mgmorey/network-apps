9e21d844da9e771fe97ce0f272b43dee2fbf0513
Michael G. Morey <mgmorey@gmail.com>
2021-10-06 09:25:16 -0500

    Fix issue in computation of object length in function get_sockaddr().

--- src/network-sockaddr.cpp
+++ src/network-sockaddr.cpp
@@ -80,17 +80,17 @@ Network::SockAddr Network::get_sockaddr(const sockaddr* addr_ptr,
 Network::SockAddr Network::get_sockaddr(const sockaddr_in* addr_ptr,
                                         socklen_type addr_len)
 {
-    const auto ptr {reinterpret_cast<const sockaddr*>(addr_ptr)};
-    assert(addr_len == sizeof *addr_ptr);
-    return get_sockaddr(ptr, addr_len);
+    const auto sa {reinterpret_cast<const sockaddr*>(addr_ptr)};
+    assert(addr_len == sizeof(sockaddr_in));
+    return get_sockaddr(sa, addr_len);
 }
 
 Network::SockAddr Network::get_sockaddr(const sockaddr_in6* addr_ptr,
                                         socklen_type addr_len)
 {
-    const auto ptr {reinterpret_cast<const sockaddr*>(addr_ptr)};
-    assert(addr_len == sizeof *addr_ptr);
-    return get_sockaddr(ptr, addr_len);
+    const auto sa {reinterpret_cast<const sockaddr*>(addr_ptr)};
+    assert(addr_len == sizeof(sockaddr_in6));
+    return get_sockaddr(sa, addr_len);
 }
 
 #ifndef _WIN32
@@ -98,9 +98,9 @@ Network::SockAddr Network::get_sockaddr(const sockaddr_in6* addr_ptr,
 Network::SockAddr Network::get_sockaddr(const sockaddr_un* addr_ptr,
                                         socklen_type addr_len)
 {
-    const auto ptr {reinterpret_cast<const sockaddr*>(addr_ptr)};
-    assert(addr_len == sizeof *addr_ptr);
-    return get_sockaddr(ptr, addr_len);
+    const auto sa {reinterpret_cast<const sockaddr*>(addr_ptr)};
+    assert(addr_len == sizeof(sockaddr_un));
+    return get_sockaddr(sa, addr_len);
 }
 
 Network::SockAddr Network::get_sockaddr(const Pathname& path)
