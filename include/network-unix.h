627a61eaac84efb681ab9a64aed2407319cfe9c4
Michael G. Morey <mgmorey@gmail.com>
2021-10-14 16:29:26 -0500

    Correct definition of macro SUN_LEN() in network-unix.h.

--- include/network-unix.h
+++ include/network-unix.h
@@ -6,7 +6,7 @@
 #include <sys/un.h>     // SUN_LEN(), sockaddr_un
 
 #ifndef SUN_LEN
-#define SUN_LEN(sun)	(sizeof *(sun) + std::strlen((sun)->sun_path))
+#define SUN_LEN(su)	(sizeof(*(su)) - sizeof((su)->sun_path) + strlen((su)->sun_path))
 #endif
 
 #endif
