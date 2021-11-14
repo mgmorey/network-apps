18ff027c70a4bb2ef4fc19c3591a7b8897f3db20
Michael G. Morey <mgmorey@gmail.com>
2021-11-14 13:42:30 -0600

    Refactor free function open() by replacing functor with a lambda.

--- src/open-endpoint.cpp
+++ src/open-endpoint.cpp
@@ -8,53 +8,35 @@
 #include <iterator>     // std::back_inserter()
 #include <variant>      // std::visit()
 
-Network::Open::Open(const OpenHandler& t_handler, bool t_verbose) :
-    m_handler(t_handler),
-    m_verbose(t_verbose)
-{
-}
-
-Network::SocketResult
-Network::Open::operator()(const Socket& t_sock) const
-{
-    auto socket_result {get_socket(t_sock, m_verbose)};
-    std::visit(Overload {
-            [&](Fd fd) {
-                socket_result = open(fd, t_sock.address());
-            },
-            [&](const Result& result) {
-                socket_result = result;
-            }
-        }, socket_result);
-    return socket_result;
-}
-
-Network::SocketResult
-Network::Open::open(Fd t_fd, const SockAddr& t_addr) const
-{
-    const auto result {Network::open(m_handler, t_fd, t_addr, m_verbose)};
-
-    if (result) {
-        return result;
-    }
-    else {
-        return t_fd;
-    }
-}
-
 Network::SocketResults
 Network::open(const OpenHandler& handler,
               const Endpoint& endpoint,
               const Hints* hints,
               bool verbose)
 {
-    SocketResults results;
+    static const auto lambda = [&](const Socket& sock) {
+        auto socket_result {get_socket(sock, verbose)};
+        std::visit(Overload {
+                [&](Fd fd) {
+                    const auto addr {sock.address()};
+                    const auto result = open(handler, fd, addr, verbose);
+
+                    if (!result) {
+                        socket_result = fd;
+                    }
+                },
+                [&](const Result&) {
+                }
+            }, socket_result);
+        return socket_result;
+    };
     const auto sockets_result {get_sockets(endpoint, hints, verbose)};
+    SocketResults results;
     std::visit(Overload {
             [&](const Sockets& sockets) {
                 std::transform(sockets.begin(), sockets.end(),
                                std::back_inserter(results),
-                               Open(handler, verbose));
+                               lambda);
             },
             [&](const Result& result) {
                 results.push_back(result);
