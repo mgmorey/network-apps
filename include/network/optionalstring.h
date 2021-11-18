a36762349d2b3b6f19d3bdc75c196cb9b5606900
Michael G. Morey <mgmorey@gmail.com>
2021-11-17 19:30:51 -0600

    Add class OptionalString defaults and annotate accessor methods.

--- include/network/optionalstring.h
+++ include/network/optionalstring.h
@@ -11,24 +11,26 @@ namespace Network
     {
     public:
         OptionalString() = default;
-        // cppcheck-suppress noExplicitConstructor
         OptionalString(const OptionalString&) = default;
+        OptionalString(OptionalString&&) = default;
         // cppcheck-suppress noExplicitConstructor
-        OptionalString(const std::nullopt_t& t_value);
+        OptionalString(const std::nullopt_t& t_value);  // NOLINT
         // cppcheck-suppress noExplicitConstructor
-        OptionalString(const std::string& t_value);
+        OptionalString(const std::string& t_value);  // NOLINT
         // cppcheck-suppress noExplicitConstructor
-        OptionalString(const char* t_value);
+        OptionalString(const char* t_value);  // NOLINT
+        ~OptionalString() = default;
         OptionalString& operator=(const OptionalString&) = default;
-        OptionalString& operator=(const std::nullopt_t& t_value);
-        OptionalString& operator=(const std::string& t_value);
-        OptionalString& operator=(const char* t_value);
-        operator std::string() const;
-        operator const char*() const;
-        bool has_value() const;
-        std::string::size_type size() const;
-        std::string value() const;
-        std::string value_or(const std::string& t_value) const;
+        OptionalString& operator=(OptionalString&&) = default;
+        OptionalString& operator=(const std::nullopt_t& t_value);  // NOLINT
+        OptionalString& operator=(const std::string& t_value);  // NOLINT
+        OptionalString& operator=(const char* t_value);  // NOLINT
+        operator std::string() const;  // NOLINT
+        operator const char*() const;  // NOLINT
+        [[nodiscard]] bool has_value() const;
+        [[nodiscard]] std::string::size_type size() const;
+        [[nodiscard]] std::string value() const;
+        [[nodiscard]] std::string value_or(const std::string& t_value) const;
 
     private:
         std::optional<std::string> m_value {};
