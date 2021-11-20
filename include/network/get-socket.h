641d885fa60391dffbe3ea0dae4ab2ca4aa830f7
Michael G. Morey <mgmorey@gmail.com>
2021-11-20 16:54:50 -0600

    Remove redundant include directive and type declaration.

--- include/network/get-socket.h
+++ include/network/get-socket.h
@@ -4,12 +4,8 @@
 #include "network/fdresult.h"       // Fd, FdResult, Result
 #include "network/socket.h"         // Socket
 
-#include <variant>      // std::variant
-
 namespace Network
 {
-    using FdResult = std::variant<Fd, Result>;
-
     extern FdResult get_socket(const Socket& sock,
                                bool verbose = false);
 }
