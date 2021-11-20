708f73209815ead6f32cbbfe264bb15713daacf3
Michael G. Morey <mgmorey@gmail.com>
2021-11-20 16:55:13 -0600

    Replace include directive and remove redudant directive and type.

--- include/network/get-socketpair.h
+++ include/network/get-socketpair.h
@@ -1,16 +1,11 @@
 #ifndef NETWORK_GET_SOCKETPAIR_H
 #define NETWORK_GET_SOCKETPAIR_H
 
-#include "network/fdpair.h"         // FdPair
+#include "network/fdpairresult.h"   // FdPairResult
 #include "network/socket.h"         // Socket
-#include "network/result.h"         // Result
-
-#include <variant>      // std::variant
 
 namespace Network
 {
-    using FdPairResult = std::variant<FdPair, Result>;
-
 #ifndef _WIN32
     extern FdPairResult get_socketpair(const Socket& sock,
                                        bool verbose = false);
