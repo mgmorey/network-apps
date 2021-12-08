b684ee07e1d60cef6a68b4cdfb1e56d20d058e89
Michael G. Morey <mgmorey@gmail.com>
2021-12-08 17:08:27 -0600

    Update include directive comment.

--- include/network/close.h
+++ include/network/close.h
@@ -1,7 +1,7 @@
 #ifndef NETWORK_CLOSE_H
 #define NETWORK_CLOSE_H
 
-#include "network/fd.h"             // Fd, fd_null, fd_type
+#include "network/fd.h"             // Fd, fd_type
 
 namespace Network
 {
