6ff242d9b8b9cc35b75b9707493366d4409c802d
Michael G. Morey <mgmorey@gmail.com>
2021-12-16 13:50:26 -0600

    Add new include file sock-arg-types.h.

---
+++ include/network/sock-arg-types.h
@@ -0,0 +1,9 @@
+#ifndef NETWORK_SOCK_ARG_TYPES_H
+#define NETWORK_SOCK_ARG_TYPES_H
+
+#include "network/family-type.h"        // family_type
+#include "network/flags-type.h"         // flags_type
+#include "network/protocol-type.h"      // protocol_type
+#include "network/socktype-type.h"      // socktype_type
+
+#endif
