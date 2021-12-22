72da33ca30762f46a8f585f0a3fd45abec2893c6
Michael G. Morey <mgmorey@gmail.com>
2021-12-22 10:13:25 -0600

    Refactor function get_sa_length() to call function get_pointer().

--- src/get-sa-length.cpp
+++ src/get-sa-length.cpp
@@ -1,12 +1,14 @@
 #include "network/get-sa-length.h"      // SockAddr, get_sa_length(),
                                         // sockaddr, sock_len_type
+#include "network/get-pointer.h"        // SockAddr, get_pointer(),
+                                        // sockaddr
 
 #include <cassert>      // assert()
 
 auto Network::get_sa_length(const SockAddr& addr) -> Network::sock_len_type
 {
 #ifdef HAVE_SOCKADDR_SA_LEN
-    const auto *const sa {reinterpret_cast<const sockaddr*>(addr.data())};
+    const auto *const sa {get_pointer(addr)};
     assert(sa != nullptr);  // NOLINT
 
     if (addr.empty()) {
