2f2b43d0d4f9c4e747a7de1c1321b68bbf0e42d1
Michael G. Morey <mgmorey@gmail.com>
2021-12-29 08:53:30 -0600

    Remove unused free functions get_byte_pointer() and get_sockaddr().

--- include/network/get-sockaddr.h
+++ include/network/get-sockaddr.h
@@ -5,23 +5,17 @@
 #include "network/sockaddr.h"           // SockAddr
 
 #ifdef _WIN32
-#include <winsock2.h>       // sockaddr, sockaddr_in
-#include <ws2tcpip.h>       // sockaddr_in6
+#include <winsock2.h>       // sockaddr
 #else
-#include <netinet/in.h>     // sockaddr_in, sockaddr_in6
 #include <sys/socket.h>     // sockaddr
 #include <sys/un.h>         // sockaddr_un
 #endif
-
-#include <optional>     // std::nullopt
 
 namespace Network
 {
     extern auto get_sockaddr() -> SockAddr;
     extern auto get_sockaddr(const sockaddr* psa,
                              std::size_t size = 0) -> SockAddr;
-    extern auto get_sockaddr(const sockaddr_in* sin) -> SockAddr;
-    extern auto get_sockaddr(const sockaddr_in6* sin6) -> SockAddr;
 #ifndef _WIN32
     extern auto get_sockaddr(const Pathname& path) -> SockAddr;
 #endif
