8381a1847fd49a2dc3c2c5e9e745033cefe21416
Michael G. Morey <mgmorey@gmail.com>
2021-12-30 17:18:10 -0600

    Remove unused overloads of free function get_sa_pointer().

--- include/network/get-sa-pointer.h
+++ include/network/get-sa-pointer.h
@@ -4,22 +4,15 @@
 #include "network/sockaddr.h"           // SockAddr
 
 #ifdef _WIN32
-#include <winsock2.h>       // sockaddr, sockaddr_in
-#include <ws2tcpip.h>       // sockaddr_in6
+#include <winsock2.h>       // sockaddr
 #else
-#include <netinet/in.h>     // sockaddr_in, sockaddr_in6
 #include <sys/socket.h>     // sockaddr
-#include <sys/un.h>         // sockaddr_un
 #endif
 
 namespace Network
 {
     extern auto get_sa_pointer(const SockAddr& addr) -> const sockaddr*;
     extern auto get_sa_pointer(SockAddr& addr) -> sockaddr*;
-    extern auto get_sa_pointer(const sockaddr_in* sin) -> const sockaddr*;
-    extern auto get_sa_pointer(sockaddr_in* sin) -> sockaddr*;
-    extern auto get_sa_pointer(const sockaddr_in6* sin6) -> const sockaddr*;
-    extern auto get_sa_pointer(sockaddr_in6* sin6) -> sockaddr*;
 }
 
 #endif
--- src/get-sa-pointer.cpp
+++ src/get-sa-pointer.cpp
@@ -13,27 +13,3 @@ auto Network::get_sa_pointer(SockAddr& addr) -> sockaddr*
     void* pointer = addr.data();
     return static_cast<sockaddr*>(pointer);
 }
-
-auto Network::get_sa_pointer(const sockaddr_in* sin) -> const sockaddr*
-{
-    const void* pointer = sin;
-    return static_cast<const sockaddr*>(pointer);
-}
-
-auto Network::get_sa_pointer(sockaddr_in* sin) -> sockaddr*
-{
-    void* pointer = sin;
-    return static_cast<sockaddr*>(pointer);
-}
-
-auto Network::get_sa_pointer(const sockaddr_in6* sin6) -> const sockaddr*
-{
-    const void* pointer = sin6;
-    return static_cast<const sockaddr*>(pointer);
-}
-
-auto Network::get_sa_pointer(sockaddr_in6* sin6) -> sockaddr*
-{
-    void* pointer = sin6;
-    return static_cast<sockaddr*>(pointer);
-}
