68ba44005b2bf0e6821d504e87f19ee5654effd7
Michael G. Morey <mgmorey@gmail.com>
2021-12-30 09:20:48 -0600

    Use constant sun_size in declaration for function get_bytespan().

--- include/network/get-bytespan.h
+++ include/network/get-bytespan.h
@@ -2,6 +2,7 @@
 #define NETWORK_GET_BYTESPAN_H
 
 #include "network/bytespan.h"           // ByteSpan
+#include "network/sizes.h"              // sun_size
 
 #ifdef _WIN32
 #include <winsock2.h>       // sockaddr
@@ -19,8 +20,7 @@ namespace Network
                              std::size_t size) -> ByteSpan;
 #ifndef _WIN32
     extern auto get_bytespan(const sockaddr_un* sun,
-                             std::size_t size =
-                             sizeof(sockaddr_un)) -> ByteSpan;
+                             std::size_t size = sun_size) -> ByteSpan;
 #endif
 }
 
