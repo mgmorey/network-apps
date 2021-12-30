80edd0c094c77fd4f6431ea1c36b363dc5705eff
Michael G. Morey <mgmorey@gmail.com>
2021-12-30 13:48:40 -0600

    Refactor function get_sa_length() to test size of first argument.

--- src/get-sa-length.cpp
+++ src/get-sa-length.cpp
@@ -1,5 +1,5 @@
 #include "network/get-sa-length.h"      // SockAddr, get_sa_length(),
-                                        // sockaddr, sock_len_type
+                                        // sock_len_type
 #include "network/get-sa-pointer.h"     // get_sa_pointer()
 
 auto Network::get_sa_length(const SockAddr& addr,
@@ -7,15 +7,14 @@ auto Network::get_sa_length(const SockAddr& addr,
 {
 #ifdef HAVE_SOCKADDR_SA_LEN
     const auto *const sa {get_sa_pointer(addr)};
-    static_cast<void>(length);
 
-    if (addr.empty()) {
-        return 0;
+    if (offsetof(sockaddr, sa_len) + sizeof sa->sa_len <= addr.size()) {
+        if (sa->sa_len > 0) {
+            length = sa->sa_len;
+        }
     }
-
-    return sa->sa_len;
 #else
     static_cast<void>(addr);
-    return length;
 #endif
+    return length;
 }
