d5958b92f21ae5344f8ae986766aff0d4bfe4e88
Michael G. Morey <mgmorey@gmail.com>
2021-12-30 18:36:19 -0600

    Refactor free function get_sa_family() and update comment.

--- src/get-sa-family.cpp
+++ src/get-sa-family.cpp
@@ -1,9 +1,15 @@
 #include "network/get-sa-family.h"      // SockAddr, family_type,
-                                        // get_sa_family(), sockaddr
-#include "network/get-sa-pointer.h"     // Sockaddr, get_sa_pointer()
+                                        // get_sa_family()
+#include "network/get-sa-pointer.h"     // get_sa_pointer(), sockaddr
 
 auto Network::get_sa_family(const SockAddr& addr) -> Network::family_type
 {
     const auto *const sa {get_sa_pointer(addr)};
-    return sa->sa_family;
+    family_type family {};
+
+    if (offsetof(sockaddr, sa_family) + sizeof sa->sa_family <= addr.size()) {
+        family = sa->sa_family;
+    }
+
+    return family;
 }
