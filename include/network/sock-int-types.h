f47db48cb294c719f0a4f426b3c28b0fc04e778d
Michael G. Morey <mgmorey@gmail.com>
2021-12-31 14:27:09 -0600

    Replace include file sock-arg-types.h with sock-int-types.h.

--- include/network/sock-int-types.h
+++ include/network/sock-int-types.h
@@ -4,6 +4,7 @@
 #include "network/family-type.h"        // family_type
 #include "network/flags-type.h"         // flags_type
 #include "network/protocol-type.h"      // protocol_type
+#include "network/sock-len-type.h"      // sock_len_type
 #include "network/socktype-type.h"      // socktype_type
 
 #endif
