8d438fb7c3b24e41f67746667118973702597cfe
Michael G. Morey <mgmorey@gmail.com>
2022-01-02 19:25:02 -0600

    Fix typo in include directive comment.

--- src/address-sin.cpp
+++ src/address-sin.cpp
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "network/address.h"            // AF_INET, INET6_ADDRSTRLEN,
+#include "network/address.h"            // AF_INET, INET_ADDRSTRLEN,
                                         // Address, in_addr
                                         // inet_ntop(), port_type,
                                         // sockaddr_in
