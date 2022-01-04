293e063bf4cb0eb9b67ab0b1bd588189a23b6dc0
Michael G. Morey <mgmorey@gmail.com>
2022-01-03 19:04:17 -0600

    Add class Address default constructors, destructor and assignment.

--- include/network/address.h
+++ include/network/address.h
@@ -38,8 +38,13 @@ namespace Network
     public:
         using value_type = Bytes;
 
+        Address(const Address& t_address) = default;
+        Address(Address&& t_address) = default;
         // cppcheck-suppress noExplicitConstructor
         Address(value_type t_value);  // NOLINT
+        ~Address() = default;
+        auto operator=(const Address& t_address) -> Address& = default;
+        auto operator=(Address&& t_address) -> Address& = default;
         auto operator=(const value_type& t_value) -> Address&;
         auto operator<(const Address& t_address) const -> bool;
         auto operator>(const Address& t_address) const -> bool;
