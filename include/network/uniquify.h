66e072fcd46e71c8fb5c9f8d96a11d76549edf68
Michael G. Morey <mgmorey@gmail.com>
2022-01-10 15:01:26 -0600

    Pass container by reference sort in template function uniquify().

--- include/network/uniquify.h
+++ include/network/uniquify.h
@@ -16,13 +16,14 @@
 #ifndef NETWORK_UNIQUIFY_H
 #define NETWORK_UNIQUIFY_H
 
-#include <algorithm>    // std::unique()
+#include <algorithm>    // std::sort(), std::unique()
 
 namespace Network
 {
     template<typename Container>
-    auto uniquify(Container cont) -> void
+    auto uniquify(Container& cont) -> void
     {
+        std::sort(cont.begin(), cont.end());
         cont.erase(std::unique(cont.begin(),
                                cont.end()),
                    cont.end());
