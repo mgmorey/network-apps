6aa62ac37d8b2b1abf6d30a42f0f3f384c8764ab
Michael G. Morey <mgmorey@gmail.com>
2022-01-12 10:47:58 -0600

    Eliminate use temporary variable in function open().

--- src/open-fd.cpp
+++ src/open-fd.cpp
@@ -28,11 +28,11 @@
 #include <iostream>     // std::cerr, std::endl
 #include <sstream>      // std::ostringstream
 
-auto Network::open(const OpenHandler& handler, Fd fd,
+auto Network::open(const OpenHandler& handler,
+                   Fd fd,
                    const Bytes& addr,
                    bool verbose) -> Network::Result
 {
-    Result result;
     assert(is_valid(addr, verbose));  // NOLINT
     const auto addr_len {get_length(addr)};
     const auto *const addr_ptr {get_sa_pointer(addr)};
@@ -65,8 +65,8 @@ auto Network::open(const OpenHandler& handler, Fd fd,
             << error
             << ": "
             << format_os_error(error);
-        result = {error, oss.str()};
+        return Result {error, oss.str()};
     }
 
-    return result;
+    return Result {};
 }
