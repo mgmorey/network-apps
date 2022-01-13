7e32811e8fb957bb3a58749523562051133ec519
Michael G. Morey <mgmorey@gmail.com>
2022-01-13 10:47:55 -0600

    Update include directive comments in source file flags.cpp.

--- src/flags.cpp
+++ src/flags.cpp
@@ -13,7 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "network/flags.h"              // Flags, operator<<()
+#include "network/flags.h"              // Flags, operator<<(),
+                                        // std::ostream
 
 #ifdef _WIN32
 #include <ws2tcpip.h>   // AI_ADDRCONFIG, AI_ALL, AI_CANONNAME,
