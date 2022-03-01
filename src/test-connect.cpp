ce302fab29af3a74165c376db1ddd30c81f337c3
Michael G. Morey <mgmorey@gmail.com>
2022-03-01 08:39:12 -0600

    Ignore OS error codes EHOSTUNREACH and ENETDOWN in test-connect.cpp.

--- src/test-connect.cpp
+++ src/test-connect.cpp
@@ -91,7 +91,12 @@ namespace TestConnect
 #if defined(WIN32)
             static const ErrorCodeSet codes {0};
 #else
-            static const ErrorCodeSet codes {EACCES, ENETUNREACH};
+            static const ErrorCodeSet codes {
+                EACCES,
+                EHOSTUNREACH,
+                ENETDOWN,
+                ENETUNREACH
+            };
 #endif
             return codes;
         }
