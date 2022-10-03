d23b9e89fec7c3c08d32bf92a8590dec3f411417
Michael G. Morey <mgmorey@gmail.com>
2022-10-03 13:32:36 -0500

    Update include directive comment in get-options.cpp.

--- src/get-options.cpp
+++ src/get-options.cpp
@@ -13,8 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "network/get-options.h"        // ArgumentSpan, Arguments,
-                                        // get_options()
+#include "network/get-options.h"        // ArgumentSpan, get_options()
 #include "network/get-option.h"         // get_optarg(), get_opterr(),
                                         // get_optind(), get_option(),
                                         // get_optopt()
