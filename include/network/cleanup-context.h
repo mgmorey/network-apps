f91fe4a75c22847d6e364468a7e1b2cce2b83dc1
Michael G. Morey <mgmorey@gmail.com>
2022-11-04 14:58:24 -0500

    Rename header guard to match preferred style.

--- include/network/cleanup-context.h
+++ include/network/cleanup-context.h
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef NETWORK_CLEANUP_H
-#define NETWORK_CLEANUP_H
+#ifndef NETWORK_CLEANUP_CONTEXT_H
+#define NETWORK_CLEANUP_CONTEXT_H
 
 #include "network/context-error-type.h"     // context_error_type
 #include "network/context.h"                // Context
