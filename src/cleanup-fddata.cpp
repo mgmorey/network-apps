b48868f2288edf74f65433bd9cc6324d68020bec
Michael G. Morey <mgmorey@gmail.com>
2022-11-10 13:04:55 -0600

    Refactor free function cleanup() with exception handling logic.

--- src/cleanup-fddata.cpp
+++ src/cleanup-fddata.cpp
@@ -14,6 +14,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "network/cleanup-fddata.h"             // FdData, cleanup()
+#include "network/error.h"                      // Error
 #include "network/get-sun-path-fddata.h"        // get_sun_path()
 #include "network/unlink.h"                     // unlink()
 
@@ -26,17 +27,23 @@ auto Network::cleanup(FdData& fd_data) -> void
         return;
     }
 
-    if (fd_data.pending()) {
-        if (const auto pathname {get_sun_path(fd_data)}) {
-            if (const auto error {unlink(*pathname, fd_data.verbose())}) {
-                if (error.number() != ENOENT) {
-                    std::cerr << *pathname
-                              << ": "
-                              << error.string()
-                              << std::endl;
+    try {
+        if (fd_data.pending()) {
+            if (const auto pathname {get_sun_path(fd_data)}) {
+                if (const auto error {unlink(*pathname, fd_data.verbose())}) {
+                    if (error.number() != ENOENT) {
+                        std::cerr << *pathname
+                                  << ": "
+                                  << error.string()
+                                  << std::endl;
+                    }
                 }
             }
         }
+    }
+    catch (const Error& error) {
+        std::cerr << error.what()
+                  << std::endl;
     }
 
     static_cast<void>(fd_data.close());
