a35976e9a2bd16f3f106e1f4833b9f191719d3af
Michael G. Morey <mgmorey@gmail.com>
2022-11-14 13:40:18 -0600

    Update include directive comments in open.h.

--- include/network/open.h
+++ include/network/open.h
@@ -16,10 +16,13 @@
 #ifndef NETWORK_OPEN_H
 #define NETWORK_OPEN_H
 
-#include "network/open-endpoint.h"      // Endpoint, OpenHandler,
-                                        // OpenResult, SocketHints,
-                                        // open()
-#include "network/open-fd.h"            // Bytes, Fd, OsErrorResult,
-                                        // open()
+#include "network/open-endpoint.h"              // OpenEndpointParams,
+                                                // OpenHandler,
+                                                // OsErrorResult,
+                                                // open()
+#include "network/open-socket.h"                // OpenHandler,
+                                                // OpenSocketParams,
+                                                // OsErrorResult,
+                                                // open()
 
 #endif
