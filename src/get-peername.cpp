047853f9611c1717d214d939aa98c617cdfc4efa
Michael G. Morey <mgmorey@gmail.com>
2022-11-14 13:40:43 -0600

    Update include directive comment in get-peername.cpp.

--- src/get-peername.cpp
+++ src/get-peername.cpp
@@ -13,10 +13,10 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "network/get-peername.h"       // Fd, GetNameHandler,
+#include "network/get-peername.h"       // GetNameHandler,
                                         // GetNameParams,
-                                        // PeerNameResult, fd_type,
-                                        // get_peername()
+                                        // PeerNameResult, Socket,
+                                        // fd_type, get_peername()
 #include "network/get-name.h"           // get_name()
 
 #ifdef WIN32
