357b3cda3781d045b320403b4bc3cfe773f9fb49
Michael G. Morey <mgmorey@gmail.com>
2023-02-07 09:52:06 -0600

    Update include directive comment in to-bytestring.sun.cpp.

--- src/to-bytestring-sun.cpp
+++ src/to-bytestring-sun.cpp
@@ -15,7 +15,6 @@
 
 #include "network/to-bytestring-sun.h"          // ByteString,
                                                 // sockaddr_un,
-                                                // sun_len_max,
                                                 // sun_len_min,
                                                 // to_bytestring()
 #include "network/get-path-pointer-sun.h"       // get_path_pointer()
