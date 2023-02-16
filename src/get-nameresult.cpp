b5d60c0e1006ec4ad3770d49c7e161c2f97aadca
Michael G. Morey <mgmorey@gmail.com>
2023-02-16 11:31:24 -0600

    Refactor free function get_nameresult().

--- src/get-nameresult.cpp
+++ src/get-nameresult.cpp
@@ -36,8 +36,8 @@ auto Network::get_nameresult(const GetNameHandler& handler,
     Network::ByteStringResult
 {
     ByteString addr {ss_size, Byte {}};
+    auto* const pointer {get_sa_pointer(addr)};
     auto length {get_length(addr)};
-    auto* pointer {get_sa_pointer(addr)};
     reset_last_context_error();
     OptionalString addr_str;
 
