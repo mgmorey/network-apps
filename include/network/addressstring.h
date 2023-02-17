cbb23d392c8ab0d6eb8ea83b3b58c720c20b45ff
Michael G. Morey <mgmorey@gmail.com>
2023-02-17 08:01:05 -0600

    Add new class AddressString.

---
+++ include/network/addressstring.h
@@ -0,0 +1,41 @@
+// Copyright (C) 2023  "Michael G. Morey" <mgmorey@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef NETWORK_ADDRESSSTRING_H
+#define NETWORK_ADDRESSSTRING_H
+
+#include "network/bytestring.h"         // ByteString
+#include "network/optionalstring.h"     // OptionalString, std::string
+
+namespace Network
+{
+    class AddressString
+    {
+    public:
+        explicit AddressString(const ByteString& t_addr) noexcept;
+        AddressString(const AddressString&) noexcept = default;
+        AddressString(AddressString&&) noexcept = default;
+        ~AddressString() noexcept = default;
+        auto operator=(const AddressString&) noexcept -> AddressString& = default;
+        auto operator=(AddressString&&) noexcept -> AddressString& = default;
+        [[nodiscard]] auto value() const -> std::string;
+
+    private:
+        ByteString m_addr;
+        mutable OptionalString m_addr_str;
+    };
+}
+
+#endif
