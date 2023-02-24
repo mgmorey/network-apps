9eac3bf0d94918e89cf63c29972333434b253d04
Michael G. Morey <mgmorey@gmail.com>
2023-02-24 10:58:30 -0600

    Update include directive comment in source file size-error.cpp.

--- src/sizeerror.cpp
+++ src/sizeerror.cpp
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "network/sizeerror.h"          // Error, SizeError
+#include "network/sizeerror.h"          // RangeError, SizeError
 
 #include <climits>      // SIZE_MAX
 #include <sstream>      // std::ostringstream
