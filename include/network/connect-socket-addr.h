71acfe732d341a1c755443da134ef1b1d540f613
Michael G. Morey <mgmorey@gmail.com>
2023-09-05 14:19:40 -0500

    Revise header guard in connect-socket-addr.h.

--- include/network/connect-socket-addr.h
+++ include/network/connect-socket-addr.h
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef NETWORK_CONNECT_SOCKET_H
-#define NETWORK_CONNECT_SOCKET_H
+#ifndef NETWORK_CONNECT_SOCKET_ADDR_H
+#define NETWORK_CONNECT_SOCKET_ADDR_H
 
 #include "network/open-socket.h"        // ByteString, OsErrorResult,
                                         // Socket
