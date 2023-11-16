7973d3e4929b18715262d3a1f1ca474d0b7df7e1
Michael G. Morey <mgmorey@gmail.com>
2023-11-16 09:23:49 -0600

    Edit include directive comment.

--- src/open-socket.cpp
+++ src/open-socket.cpp
@@ -13,10 +13,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "network/open-socket.h"                // ByteString, OpenHandler,
-                                                // OsErrorResult, Socket,
-                                                // descriptor_type, open(),
-                                                // operator<<()
+#include "network/open-socket.h"                // open()
 #include "network/addressstring.h"              // AddressString
 #include "network/descriptor-type.h"            // descriptor_type
 #include "network/format-os-error.h"            // format_os_error()
