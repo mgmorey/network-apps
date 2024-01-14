19db553449374cb753a5b8cc0068c7c253f878fe
Michael G. Morey <mgmorey@gmail.com>
2024-01-14 11:34:28 -0600

    Remove tests for empty address string from program test-address.cpp.

--- src/test-address.cpp
+++ src/test-address.cpp
@@ -144,18 +144,6 @@ namespace TestAddress
         }
     }
 
-    auto test_empty(const ByteString& addr) -> void
-    {
-        assert(addr.empty());
-        assert(get_sa_family(addr) == AF_UNSPEC);
-        assert(get_sa_length(addr) == 0);
-    }
-
-    auto test_empty() -> void
-    {
-        test_empty({});
-    }
-
     auto test_invalid_sa_family() -> void
     {
         std::string actual_error_str;
@@ -363,7 +351,6 @@ auto main(int argc, char* argv[]) -> int
         }
 
         test_valid();
-        test_empty();
         test_invalid_sa_family();
         test_invalid_sin_family();
         test_invalid_sin_length();
