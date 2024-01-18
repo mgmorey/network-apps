87c077b5558816808cf4ea274824c6f14a2f1b3a
Michael G. Morey <mgmorey@gmail.com>
2024-01-18 09:31:11 -0600

    Restore original initializer to function create_bytestring().

--- src/create-bytestring.cpp
+++ src/create-bytestring.cpp
@@ -14,10 +14,11 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "network/create-bytestring.h"  // create_bytestring()
+#include "network/byte.h"               // Byte
 #include "network/bytestring.h"         // ByteString
 #include "network/sa-len-limits.h"      // sa_len_max
 
 auto Network::create_bytestring() -> ByteString
 {
-    return ByteString {sa_len_max, {}};
+    return ByteString {sa_len_max, Byte {}};
 }
