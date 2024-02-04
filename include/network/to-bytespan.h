35c37924898dabecb6682b3a2f7ee6636cdf0500
Michael G. Morey <mgmorey@gmail.com>
2024-02-03 18:33:11 -0600

    Normalize header guard to preferred style in to-bytespan.h.

--- include/network/to-bytespan.h
+++ include/network/to-bytespan.h
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef NETWORK_TO_BYTESPAN_VOID_H
-#define NETWORK_TO_BYTESPAN_VOID_H
+#ifndef NETWORK_TO_BYTESPAN_H
+#define NETWORK_TO_BYTESPAN_H
 
 #include "network/bytespan.h"           // ByteSpan
 
