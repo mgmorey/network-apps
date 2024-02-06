4bfd4d9d39b0aa941053ab657e9588f1092e0845
Michael G. Morey <mgmorey@gmail.com>
2024-02-06 07:57:16 -0600

    Refactor overloads of to_bytestring(), renaming include/source files.

---
+++ include/network/to-bytestring-auto.h
@@ -0,0 +1,41 @@
+// Copyright (C) 2024  "Michael G. Morey" <mgmorey@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef NETWORK_TO_BYTESTRING_PTR_H
+#define NETWORK_TO_BYTESTRING_PTR_H
+
+#include "network/bytestring.h"                 // ByteString
+#include "network/sa-len-type.h"                // sa_len_type
+#include "network/to-bytespan.h"                // to_bytespan()
+#include "network/to-bytestring-span.h"         // to_bytestring()
+#include "network/validate.h"                   // validate()
+
+namespace Network
+{
+    auto to_bytestring(const auto* pointer,
+                       sa_len_type length) -> Network::ByteString
+    {
+        validate(pointer, length);
+        return to_bytestring(to_bytespan(pointer, length));
+    }
+
+    auto to_bytestring(const auto* pointer) -> Network::ByteString
+    {
+        validate(pointer);
+        return to_bytestring(to_bytespan(pointer, sizeof *pointer));
+    }
+}
+
+#endif
---
+++ src/to-bytestring-char.cpp
@@ -0,0 +1,54 @@
+// Copyright (C) 2022  "Michael G. Morey" <mgmorey@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include "network/to-bytestring-char.h"         // to_bytestring()
+#include "network/bytestring.h"                 // ByteString
+#include "network/get-path-pointer-sun.h"       // get_path_pointer()
+#include "network/os-features.h"                // HAVE_SOCKADDR_SA_LEN
+#include "network/sun-len-limits.h"             // sun_len_min
+#include "network/sun-len-type.h"               // sun_len_type
+#include "network/to-bytestring-auto.h"         // to_bytestring()
+
+#ifndef WIN32
+#include <sys/un.h>         // sockaddr_un
+#endif
+
+#ifndef WIN32
+
+#include <sys/socket.h>     // AF_UNIX
+
+#include <cstring>      // ::strlen(), ::strncpy()
+
+auto Network::to_bytestring(const char* path) ->
+    Network::ByteString
+{
+    sockaddr_un sun {};
+    sun_len_type sun_len {sun_len_min};
+
+    if (path != nullptr) {
+        const auto path_len {::strnlen(path, sizeof sun.sun_path - 1)};
+        auto* sun_path {get_path_pointer(&sun)};
+        ::strncpy(sun_path, path, path_len);
+        sun_len += path_len;
+    }
+
+    sun.sun_family = AF_UNIX;
+#ifdef HAVE_SOCKADDR_SA_LEN
+    sun.sun_len = sun_len;
+#endif
+    return to_bytestring(&sun, sun_len);
+}
+
+#endif
