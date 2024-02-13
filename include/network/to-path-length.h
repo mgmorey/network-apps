1423cfb1a2ecf09e4d070de36f35eb2404f3ac76
Michael G. Morey <mgmorey@gmail.com>
2024-02-13 11:15:05 -0600

    Modify header guard in to-path-length.h to follow preferred style.

--- include/network/to-path-length.h
+++ include/network/to-path-length.h
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef NETWORK_TO_PATH_LEN_H
-#define NETWORK_TO_PATH_LEN_H
+#ifndef NETWORK_TO_PATH_LENGTH_H
+#define NETWORK_TO_PATH_LENGTH_H
 
 #include "network/path-length-limits.h"         // path_length_max,
                                                 // path_length_min
