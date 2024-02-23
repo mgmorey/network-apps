56f0dbdd04ab12ad07708a9caaa5f00174d77451
Michael G. Morey <mgmorey@gmail.com>
2024-02-22 20:23:45 -0600

    Refactor overload of free function validate() for type ByteString.

--- src/validate-bs.cpp
+++ src/validate-bs.cpp
@@ -17,24 +17,9 @@
 #include "network/bytestring.h"                 // ByteString
 #include "network/familyerror.h"                // FamilyError
 #include "network/get-sa-family.h"              // get_sa_family()
-#include "network/get-sa-size-maximum.h"        // get_sa_size_maximum()
-#include "network/get-sa-size-minimum.h"        // get_sa_size_minimum()
-#include "network/os-features.h"                // HAVE_SOCKADDR_SA_LEN
-#include "network/sa-len-limits.h"              // sa_len_min
-#include "network/salengtherror.h"              // SaLengthError
-#include "network/sin6lengtherror.h"            // Sin6LengthError
-#include "network/sinlengtherror.h"             // SinLengthError
-
-#ifdef HAVE_SOCKADDR_SA_LEN
-#include "network/get-sa-length.h"              // get_sa_length()
-#endif
-
-#ifndef WIN32
-#include "network/get-sun-length.h"             // get_sun_length()
+#include "network/get-sin6-pointer.h"           // get_sin6_pointer()
+#include "network/get-sin-pointer.h"            // get_sin_pointer()
 #include "network/get-sun-pointer.h"            // get_sun_pointer()
-#include "network/sun-len-limits.h"             // sun_len_max
-#include "network/sunlengtherror.h"             // SunLengthError
-#endif
 
 #ifdef WIN32
 #include <winsock2.h>       // AF_INET, AF_INET6, AF_UNIX, AF_UNSPEC
@@ -42,67 +27,23 @@
 #include <sys/socket.h>     // AF_INET, AF_INET6, AF_UNIX, AF_UNSPEC
 #endif
 
-#include <string>       // std::to_string()
-#include <utility>      // std::cmp_greater(), std::cmp_less(),
-                        // std::cmp_not_equal()
-
 auto Network::validate(const ByteString& addr) -> void
 {
-    const auto family {get_sa_family(addr)};
-
-    switch (family) {
+    switch (get_sa_family(addr)) {
 #ifndef WIN32
     case AF_UNIX:
+        static_cast<void>(get_sun_pointer(addr));
+        break;
 #endif
     case AF_INET:
+        static_cast<void>(get_sin_pointer(addr));
+        break;
     case AF_INET6:
+        static_cast<void>(get_sin6_pointer(addr));
+        break;
+    case AF_UNSPEC:
         break;
     default:
         throw FamilyError();
     }
-
-    const auto size {addr.size()};
-    const auto size_max {get_sa_size_maximum(family)};
-    const auto size_min {get_sa_size_minimum(family)};
-
-    if (size < size_min || size > size_max) {
-        switch (family) {
-#ifndef WIN32
-        case AF_UNIX:
-            throw SunLengthError(std::to_string(size), size_max);
-            break;
-#endif
-        case AF_INET:
-            throw SinLengthError(std::to_string(size), size_min, size_max);
-            break;
-        case AF_INET6:
-            throw Sin6LengthError(std::to_string(size), size_min, size_max);
-            break;
-        default:
-            throw FamilyError();
-        }
-    }
-
-#ifdef HAVE_SOCKADDR_SA_LEN
-
-    const auto sa_len {get_sa_length(addr)};
-
-    if (std::cmp_not_equal(sa_len, size)) {
-        throw SaLengthError(std::to_string(sa_len), size, size);
-    }
-
-#endif
-#ifndef WIN32
-
-    if (family == AF_UNIX) {
-        const auto* const sun {get_sun_pointer(addr)};
-        const auto sun_len {get_sun_length(sun, sun_len_max)};
-
-        if (std::cmp_less(sun_len, size_min) ||
-            std::cmp_greater(sun_len, size_max)) {
-            throw SunLengthError(std::to_string(sun_len), size_max);
-        }
-    }
-
-#endif
 }
