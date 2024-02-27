49d0cbc7d9a5e695173f3d4fd0bb2e64eff5796c
Michael G. Morey <mgmorey@gmail.com>
2024-02-27 07:31:51 -0600

    Sort include directives in source file validate-bs.cpp.

--- src/validate-bs.cpp
+++ src/validate-bs.cpp
@@ -17,8 +17,8 @@
 #include "network/bytestring.h"                 // ByteString
 #include "network/familyerror.h"                // FamilyError
 #include "network/get-sa-family.h"              // get_sa_family()
-#include "network/get-sin6-pointer.h"           // get_sin6_pointer()
 #include "network/get-sin-pointer.h"            // get_sin_pointer()
+#include "network/get-sin6-pointer.h"           // get_sin6_pointer()
 #include "network/get-sun-pointer.h"            // get_sun_pointer()
 
 #ifdef WIN32
