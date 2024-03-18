daad56fb75846131e9226fe21dfc5ac27817e1ee
Michael G. Morey <mgmorey@gmail.com>
2024-03-18 14:27:14 -0500

    Return std::nullopt for sa_family value of AF_UNIX in to_path().

--- src/to-path-bytestring.cpp
+++ src/to-path-bytestring.cpp
@@ -13,23 +13,26 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#ifndef WIN32
+
 #include "network/to-path-bytestring.h"         // to_path()
 #include "network/bytestring.h"                 // ByteString
 #include "network/get-path-pointer-bs.h"        // get_path_pointer()
+#include "network/get-sa-family.h"              // get_sa_family()
 #include "network/optionalpathname.h"           // OptionalPathname
 #include "network/sun-len-limits.h"             // sun_len_min
 
-#ifndef WIN32
 #include <sys/socket.h>     // AF_UNIX
-#endif
 
 #include <cstring>      // ::strnlen()
 #include <string>       // std::string
 
-#ifndef WIN32
-
 auto Network::to_path(const ByteString& addr) -> OptionalPathname
 {
+    if (get_sa_family(addr) != AF_UNIX) {
+        return {};
+    }
+
     const auto* const data {get_path_pointer(addr)};
     const auto size_max {addr.size() - sun_len_min};
     const auto size {::strnlen(data, size_max)};
