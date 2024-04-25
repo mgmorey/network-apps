883b5a7fe82d42dfe9018200574bbc388c093e7f
Michael G. Morey <mgmorey@gmail.com>
2024-04-25 15:42:39 -0500

    Rename class, include and source files beginning with "addrinfo."

--- include/network/addresslist.h
+++ include/network/addresslist.h
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef NETWORK_ADDRINFO_H
-#define NETWORK_ADDRINFO_H
+#ifndef NETWORK_ADDRESSLIST_H
+#define NETWORK_ADDRESSLIST_H
 
 #include "network/optionalhints.h"      // OptionalHints
 #include "network/optionalhostname.h"   // OptionalHostname
