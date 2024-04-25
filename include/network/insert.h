5d02adba4ef118999da8a80ee6c18edb89fa2026
Michael G. Morey <mgmorey@gmail.com>
2024-04-25 16:59:42 -0500

    Normalize header guard in include file insert.h.

--- include/network/insert.h
+++ include/network/insert.h
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef NETWORK_INSERT_ADDRINFO_H
-#define NETWORK_INSERT_ADDRINFO_H
+#ifndef NETWORK_INSERT_H
+#define NETWORK_INSERT_H
 
 #include "network/addresslist.h"        // AddressList
 #include "network/optionalhints.h"      // OptionalHints
