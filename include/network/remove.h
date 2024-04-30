2f5f6bace39a2e537a5a725b96f04fba5f47a42c
Michael G. Morey <mgmorey@gmail.com>
2024-04-30 13:31:06 -0500

    Modify header guard in remote.h to follow preferred style.

--- include/network/remove.h
+++ include/network/remove.h
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef NETWORK_REMOVE_SOCKET_H
-#define NETWORK_REMOVE_SOCKET_H
+#ifndef NETWORK_REMOVE_H
+#define NETWORK_REMOVE_H
 
 #include "network/pathname.h"                   // Pathname
 
