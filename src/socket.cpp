125b140ca6116f013047df35f7fc1872c272d5aa
Michael G. Morey <mgmorey@gmail.com>
2024-05-02 17:38:22 -0500

    Rename class Socket constructor parameter m_handle to m_socket.

--- src/socket.cpp
+++ src/socket.cpp
@@ -21,8 +21,8 @@
 #include "network/socketdata.h"                 // SocketData
 #include "network/sockethints.h"                // SocketHints
 
-Network::Socket::Socket(socket_type t_handle, bool t_verbose) :
-    m_socket_data(new SocketData {t_handle, t_verbose})
+Network::Socket::Socket(socket_type t_socket, bool t_verbose) :
+    m_socket_data(new SocketData {t_socket, t_verbose})
 {
 }
 
