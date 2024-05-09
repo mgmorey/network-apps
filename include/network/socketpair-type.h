d763126af9d2c83ea4a737430094d0c2d436b694
Michael G. Morey <mgmorey@gmail.com>
2024-05-08 19:46:05 -0500

    Add type socketpair_type.

---
+++ include/network/socketpair-type.h
@@ -0,0 +1,31 @@
+// Copyright (C) 2024  "Michael G. Morey" <mgmorey@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef NETWORK_SOCKETPAIR_TYPE_H
+#define NETWORK_SOCKETPAIR_TYPE_H
+
+#include "network/socket-type.h"                // socket_type
+#include "network/socketpair-size.h"            // socketpair_size
+
+#include <array>        // std::array
+
+namespace Network
+{
+#ifndef WIN32
+    using socketpair_type = std::array<socket_type, socketpair_size>;
+#endif
+}
+
+#endif
