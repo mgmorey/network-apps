c779cb12dc1b8319aa1ca349fd31f0a1024c8cca
Michael G. Morey <mgmorey@gmail.com>
2024-08-29 15:55:02 -0500

    Qualify context as const in free function get_unique_context().

--- src/get-unique-context.cpp
+++ src/get-unique-context.cpp
@@ -25,7 +25,7 @@ auto Network::get_unique_context(const OptionalVersion& t_version,
                                  FailureMode t_failure,
                                  bool t_is_verbose) -> UniqueContext
 {
-    auto context
+    const auto context
     {
         std::make_unique<SimpleContext>(t_version, t_failure, t_is_verbose)
     };
