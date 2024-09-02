6a4f94042883a4f4c66f7e4c979979f7f041146a
Michael G. Morey <mgmorey@gmail.com>
2024-09-01 20:08:27 -0500

    Revise header guard in get-context.h to follow preferred style.

--- include/network/get-context.h
+++ include/network/get-context.h
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef NETWORK_GET_SHARED_CONTEXT_H
-#define NETWORK_GET_SHARED_CONTEXT_H
+#ifndef NETWORK_GET_CONTEXT_H
+#define NETWORK_GET_CONTEXT_H
 
 #include "network/failuremode.h"        // FailureMode
 #include "network/optionalversion.h"    // OptionalVersion
