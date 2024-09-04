fd968b3371a3c0a86b056a7d99db0000d0596f03
Michael G. Morey <mgmorey@gmail.com>
2024-09-04 13:56:52 -0500

    Refactor overload of free function bind() for type Socket.

--- src/bind-socket-bs.cpp
+++ src/bind-socket-bs.cpp
@@ -15,21 +15,10 @@
 
 #include "network/bind-socket-bs.h"             // bind()
 #include "network/bytestring.h"                 // ByteString
-#include "network/open-socket.h"                // open()
-#include "network/openhandler.h"                // OpenHandler
-#include "network/opensocketparams.h"           // OpenSocketParams
 #include "network/oserrorresult.h"              // OsErrorResult
 #include "network/socket.h"                     // Socket
 
-#ifdef WIN32
-#include <winsock2.h>       // ::bind()
-#else
-#include <sys/socket.h>     // ::bind()
-#endif
-
 auto Network::bind(const Socket& sock, const ByteString& addr) -> OsErrorResult
 {
-    const OpenHandler handler {::bind, "::bind"};
-    const OpenSocketParams args {sock, addr};
-    return open(handler, args);
+    return sock->bind(addr);
 }
