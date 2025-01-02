067c6ec51d6ed3281f6affe49dda9ba33c91683c
Michael G. Morey <mgmorey@gmail.com>
2025-01-02 10:18:49 -0600

    Modify header guard in socketpair.h to follow preferred style.

--- include/unix/network/socketpair.h
+++ include/unix/network/socketpair.h
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#ifndef NETWORK_SOCKETPAIR_H
-#define NETWORK_SOCKETPAIR_H
+#ifndef UNIX_NETWORK_SOCKETPAIR_H
+#define UNIX_NETWORK_SOCKETPAIR_H
 
 #include "network/uniquesocket.h"       // UniqueSocket
 
