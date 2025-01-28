d4c9dd8b722e7286ef1c69ed9f80c76aba6532a0
Michael Morey <michael.morey@lseg.com>
2025-01-28 10:09:25 -0600

    Add new script format-gcc-option.py and rename format-gcc-option.

---
+++ script/format-gcc-option.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# format-gcc-option: format compiler option for assembler or linker
+# Copyright (C) 2025  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import sys
+
+if (len(sys.argv) < 2):
+    exit(1)
+
+tool = sys.argv[1]
+
+if (tool != "as" and tool != "ld"):
+    exit(1)
+
+option = '-W%.1s,%s' % (tool, ','.join(sys.argv[2:]))
+print(option.replace('$', '\\$'))
