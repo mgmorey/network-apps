43d83d1f7572749b27be94d350e3753b17ecd3e1
Michael G. Morey <mgmorey@gmail.com>
2025-02-25 13:57:19 -0600

    Adjust indentation of include directive comments in validate-bs.cpp.

--- src/validate-bs.cpp
+++ src/validate-bs.cpp
@@ -13,14 +13,14 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#include "network/validate-bs.h"                // validate
-#include "network/bytestring.h"                 // ByteString
-#include "network/familyerror.h"                // FamilyError
-#include "network/get-sa-family.h"              // get_sa_family()
-#include "network/get-sin-pointer.h"            // get_sin_pointer()
-#include "network/get-sin6-pointer.h"           // get_sin6_pointer()
+#include "network/validate-bs.h"        // validate
+#include "network/bytestring.h"         // ByteString
+#include "network/familyerror.h"        // FamilyError
+#include "network/get-sa-family.h"      // get_sa_family()
+#include "network/get-sin-pointer.h"    // get_sin_pointer()
+#include "network/get-sin6-pointer.h"   // get_sin6_pointer()
 #ifndef WIN32
-#include "network/get-sun-pointer.h"            // get_sun_pointer()
+#include "network/get-sun-pointer.h"    // get_sun_pointer()
 #endif
 
 #ifdef WIN32
