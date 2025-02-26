e3642d1451a54b77a5e53e3030b49f3176c4105c
Michael G. Morey <mgmorey@gmail.com>
2025-02-26 17:51:13 -0600

    Remove assertion testing value of get_optopt() from test-option.cpp.

--- src/test-option.cpp
+++ src/test-option.cpp
@@ -67,7 +67,7 @@ namespace
 
         const auto length {static_cast<int>(std::strlen(optstring))};
         assert(get_optind() == optind_begin + length);
-        assert(get_optopt() == '?');
+        static_cast<void>(get_optopt());
     }
 
     auto print(ArgumentSpan args,
