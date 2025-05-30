d671ea237ed0e4e2cbe2562245084539f1053f39
Michael G. Morey <mgmorey@gmail.com>
2025-05-30 10:37:29 -0500

    Add new include file failmode.hpp.

---
+++ include/network/failmode.hpp
@@ -0,0 +1,30 @@
+// Copyright (C) 2025  "Michael G. Morey" <mgmorey@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef NETWORK_FAILMODE_HPP
+#define NETWORK_FAILMODE_HPP
+
+#include <cstdint>      // std::uint8_t
+
+namespace Network
+{
+    enum class FailMode : std::uint8_t {
+        throw_error,
+        return_error,
+        return_zero
+    };
+}
+
+#endif
