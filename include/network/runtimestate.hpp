7ea016661b9660804a1fe567036dee06d6b4757a
Michael G. Morey <mgmorey@gmail.com>
2025-05-30 10:37:39 -0500

    Add new include file runtimestate.hpp.

---
+++ include/network/runtimestate.hpp
@@ -0,0 +1,36 @@
+// Copyright (C) 2025  "Michael G. Morey" <mgmorey@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef UNIX_NETWORK_RUNTIMESTATE_HPP
+#define UNIX_NETWORK_RUNTIMESTATE_HPP
+
+#ifndef WIN32
+
+#include "network/failuremode.hpp"      // FailureMode
+
+namespace Network
+{
+    struct RuntimeState
+    {
+        int m_error_code {0};
+        FailureMode m_failure_mode {FailureMode::throw_error};
+        bool m_is_started {false};
+        bool m_is_verbose {false};
+    };
+}
+
+#endif
+
+#endif
