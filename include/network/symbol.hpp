d89948dece4ea932e914ea61f5a1f54d8e6ffd2e
Michael G. Morey <mgmorey@gmail.com>
2025-05-30 12:50:40 -0500

    Add new include file symbol.hpp.

---
+++ include/network/symbol.hpp
@@ -0,0 +1,31 @@
+// Copyright (C) 2025  "Michael G. Morey" <mgmorey@gmail.com>
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef NETWORK_SYMBOL_HPP
+#define NETWORK_SYMBOL_HPP
+
+#include <cstdint>      // std::uint8_t
+
+namespace Network
+{
+    enum class Symbol : std::uint8_t {
+        bind,
+        connect,
+        peername,
+        sockname
+    };
+}
+
+#endif
