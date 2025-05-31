79ce468e4857b748d7c6368c4bab21d6e4bcd7f9
Michael G. Morey <mgmorey@gmail.com>
2025-05-31 14:56:42 -0500

    Remove extraneous character at start of file test-runtime.cpp.

--- src/test-runtime.cpp
+++ src/test-runtime.cpp
@@ -1,4 +1,4 @@
-g// Copyright (C) 2022  "Michael G. Morey" <mgmorey@gmail.com>
+// Copyright (C) 2022  "Michael G. Morey" <mgmorey@gmail.com>
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
