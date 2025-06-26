cb050fae14c0893abfa1b5c863d85a3c46e0b9df
Michael G. Morey <mgmorey@gmail.com>
2025-06-25 19:16:56 -0500

    Omit flag for default optimization level 0 from Default.mk.

--- gmake/flags/build/Default.mk
+++ gmake/flags/build/Default.mk
@@ -15,10 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-ifeq "$(filter -O%,$(CXXFLAGS))" ""
-CXXFLAGS += -O0
-endif
-
 ifeq "$(filter -g%,$(CXXFLAGS))" ""
 CXXFLAGS += -g
 endif
