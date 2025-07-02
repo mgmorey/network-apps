9ca61c1675f60c3889b4f5dcb508956d5321ab26
Michael G. Morey <mgmorey@gmail.com>
2025-07-02 10:22:45 -0500

    Replace option --html-details with -html-nested in common-flags.mk.

--- gmake/flags/common-flags.mk
+++ gmake/flags/common-flags.mk
@@ -115,12 +115,12 @@ CXXFLAGS += $(call join-flags,-Wa,$(asflags))
 
 # GCOVRFLAGS
 
-GCOVRFLAGS_HTML = --html-details --html-theme=$(HTML_THEME)     \
---html-title='$(html_title)' $(foreach                          \
+GCOVRFLAGS_HTML = --html-nested --html-theme=$(HTML_THEME)	\
+--html-title='$(html_title)' $(foreach				\
 file,$^,--json-add-tracefile=$(file))
 
-GCOVRFLAGS = --exclude-unreachable-branches $(if        \
-$(GCOV),--gcov-executable='$(GCOV)',) $(if $(filter     \
+GCOVRFLAGS = --exclude-unreachable-branches $(if	\
+$(GCOV),--gcov-executable='$(GCOV)',) $(if $(filter	\
 %.json,$@),--json,$(GCOVRFLAGS_HTML)) --output=$@
 
 # LDFLAGS
