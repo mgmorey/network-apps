381e5128293e924a95d58479d7846ca7aaa3f31b
Michael G. Morey <mgmorey@gmail.com>
2025-09-04 09:07:27 -0500

    Refactor function connect() in unix-client.cpp.

--- src/unix/unix-client.cpp
+++ src/unix/unix-client.cpp
@@ -35,6 +35,7 @@
 #include <iomanip>      // std::right, std::setw()
 #include <iostream>     // std::cerr, std::cout, std::endl
 #include <string>       // std::string
+#include <utility>      // std::move()
 
 namespace
 {
@@ -58,36 +59,33 @@ namespace
 
     auto connect()
     {
-        UniqueSocket s;
+        auto result {connect(SOCKET_NAME, SOCKET_HINTS, is_verbose)};
 
-        if (auto result {connect(SOCKET_NAME, SOCKET_HINTS, is_verbose)}) {
-            s.swap(result.value());
-
-            const Address host {s->get_name(Symbol::connect)};
-            const Address peer {s->get_peername()};
-            const Address self {s->get_sockname()};
-
-            if (is_verbose) {
-                // clang-format off
-                std::cout << "Socket "
-                          << std::right << std::setw(handle_width) << *s
-                          << " connected "
-                          << self
-                          << std::endl
-                          << std::right << std::setw(indent_width) << "to "
-                          << peer
-                          << std::endl;
-                // clang-format on
-            }
-
-            assert(host.text() == peer.text());
-        }
-        else {
+        if (!result) {
             std::cerr << result.error().string() << std::endl;
             std::exit(EXIT_FAILURE);
         }
 
-        return s;
+        auto& s {result.value()};
+        const Address host {s->get_name(Symbol::connect)};
+        const Address peer {s->get_peername()};
+
+        if (is_verbose) {
+            const Address self {s->get_sockname()};
+            // clang-format off
+            std::cout << "Socket "
+                      << std::right << std::setw(handle_width) << *s
+                      << " connected "
+                      << self
+                      << std::endl
+                      << std::right << std::setw(indent_width) << "to "
+                      << peer
+                      << std::endl;
+            // clang-format on
+        }
+
+        assert(host.text() == peer.text());
+        return std::move(s);
     }
 
     auto parse(int argc, char** argv)
